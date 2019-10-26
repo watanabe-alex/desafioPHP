5d8e17cd1dd25b8f6e509cfe09bf82c7e7fffec6
Alex Watanabe <aowatanabe@gmail.com>
2019-10-26 18:56:53 -0300

    teminando front-end

--- pagina-individual.php
+++ pagina-individual.php
@@ -9,35 +9,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Produto</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-    <main>
-        <div class="container fundo-cinza">
-            <div class="row">
-                <button type="button" class="btn btn-light">Voltar para lista de produtos</button>
-            </div>
-            <div class="row">
-                <img class="col-5" src="img/thebeatles.jpg" alt="imagem-do-produto">
-                <div class="col-7 d-flex justify-content-center">
-                    <h2>The Beatles</h2>
-                    <p>Categoria</p>
+    <main class="d-flex justify-content-center">
+
+        <div class="container fundo-cinza m-5">
+
+            <!-- botão -->
+            <button type="button" class="btn btn-light ml-5">Voltar para lista de produtos</button>
+
+            <!-- imagem e informações -->
+            <div class="row mt-5 ml-5">
+
+                <!-- imagem -->
+                <div class="col-5 pl-0">
+                    <img src="img/thebeatles.jpg" alt="imagem-do-produto">
+                </div>
+                <!-- informações -->
+                <div class="col-6">
+                    <h2 class="h1 pb-3">The Beatles</h2>
+                    <p class="texto-label">Categoria</p>
                     <p>Camiseta</p>
-                    <p>Descrição</p>
+                    <p class="texto-label">Descrição</p>
                     <p>Camiseta branca, 100% algodão com estampa preta.</p>
-                    <div class="d-flex">
-                        <div>
-                            <p>Quantidade em estoque</p>
+                    <div class="row pt-4">
+                        <div class="col-6">
+                            <p class="texto-label">Quantidade em estoque</p>
                             <p>100</p>
                         </div>
-                        <div>
-                            <p>Preço</p>
+                        <div class="col-6">
+                            <p class="texto-label">Preço</p>
                             <p><b>R$ 55,00</b></p>
                         </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
+
     </main>
 
 </body>
